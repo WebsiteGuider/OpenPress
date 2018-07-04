a324c38c18d5912e0085f60267b65dc42421d21a
Raashid din dar <34117119+raadin7@users.noreply.github.com>
2018-07-04 20:09:28 +0530

    Update index.php

--- index.php
+++ index.php
@@ -16,10 +16,15 @@ switch ( $action ) {
 
 function archive() {
   $results = array();
-  $data = Article::getList();
+  $categoryId = ( isset( $_GET['categoryId'] ) && $_GET['categoryId'] ) ? (int)$_GET['categoryId'] : null;
+  $results['category'] = Category::getById( $categoryId );
+  $data = Article::getList( 100000, $results['category'] ? $results['category']->id : null );
   $results['articles'] = $data['results'];
-  $results['totalRows'] = $data['totalRows'];
-  $results['pageTitle'] = "Article Archive | wgCMS";
+  $data = Category::getList();
+  $results['categories'] = array();
+  foreach ( $data['results'] as $category ) $results['categories'][$category->id] = $category;
+  $results['pageHeading'] = $results['category'] ?  $results['category']->name : "Article Archive";
+  $results['pageTitle'] = $results['pageHeading'] . " | Widget News";
   require( CONTENT_PATH . "/archive.php" );
 }
 
@@ -31,6 +36,7 @@ function viewArticle() {
 
   $results = array();
   $results['article'] = Article::getById( (int)$_GET["articleId"] );
+  $results['category'] = Category::getById( $results['article']->categoryId );
   $results['pageTitle'] = $results['article']->title . " | wgCMS";
   require( CONTENT_PATH . "/viewArticle.php" );
 }
@@ -40,6 +46,9 @@ function homepage() {
   $data = Article::getList( HOMEPAGE_NUM_ARTICLES );
   $results['articles'] = $data['results'];
   $results['totalRows'] = $data['totalRows'];
+  $data = Category::getList();
+  $results['categories'] = array();
+  foreach ( $data['results'] as $category ) $results['categories'][$category->id] = $category; 
   $results['pageTitle'] = "wgCMS";
   require( CONTENT_PATH . "/homepage.php" );
 }
